952511fec9a6ee42d3831914e00799373b6ba3ba
MauricioTorres111 <79350614+MauricioTorres111@users.noreply.github.com>
2021-05-07 23:56:24 -0500

    Add files via upload

---
+++ Practica 4/Practica4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Mauricio Alejandro Torres Luna
+
+base64 msg.txt
+base64 fcfm.png > fcfm.txt
+base64 -d mystery_img1.txt > mystery_img1.jpg
+base64 -d mystery_img2.txt > mystery_img2.jpg
