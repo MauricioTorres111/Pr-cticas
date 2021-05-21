71fa08318d4a64e27f5c2af1197609ffd3512f3f
MauricioTorres111 <79350614+MauricioTorres111@users.noreply.github.com>
2021-05-20 19:12:42 -0500

    Add files via upload

---
+++ Practica7.py
@@ -0,0 +1,18 @@
+# Practica 7 Obtener Archivos con FTP
+# Mauricio Alejandro Torres Luna
+
+import ftplib
+
+def Cliente_Conexion(Servidor, Name, email):
+    ftp = ftplib.FTP(Servidor, Name, email)
+    #Se listaran los archivos encontrados en el directorio
+    ftp.cwd('/pub')
+    print('Archivos disponibles en el servidor ', Servidor )
+    archivos = ftp.dir()
+    print(archivos)
+    ftp.quit()
+
+Ruta_Servidor_FTP = 'ftp.kernel.org'
+
+if __name__ == '__main__':
+    Cliente_Conexion(Servidor=Ruta_Servidor_FTP, Name='anonymous', Email='nobody@nourl.com')
